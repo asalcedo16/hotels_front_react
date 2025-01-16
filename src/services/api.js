5fa3d734f5584d3a3b60089639b6a8b3cd5c94be
Alvaro E. Salcedo <asalcedoc@alltimetech.com.co>
2025-01-16 11:04:13 -0500

    creacion de variable de entorno

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configura la URL base de la API
 const api = axios.create({
-  baseURL: 'https://bjnic1crs0.execute-api.us-east-1.amazonaws.com/api/', // Cambia esto según tu backend
+  baseURL: process.env.REACT_APP_API_URL, // Cambia esto según tu backend
 });
 
 export default api;
